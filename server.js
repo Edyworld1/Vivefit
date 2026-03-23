99363a6c981fa856362ad2d59ec1971db7345e1e
Edmundo Salas <edmundomendez117@gmail.com>
2026-03-23 11:03:47 -0600

    Agregando ruta raíz

--- server.js
+++ server.js
@@ -132,10 +132,6 @@ usuario.password = hashedPassword;
 // ENCENDER SERVIDOR
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-  res.send("Servidor funcionando 🚀");
-});
-
 app.listen(PORT, () => {
     console.log(`🚀 Servidor listo en: http://localhost:${PORT}`);
 });
